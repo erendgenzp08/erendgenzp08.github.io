fa16fa55e62e2c6603d478233a69fd0dbc79b90f
erendgenzp08 <erendgenzp08@gmail.com>
2026-04-05 21:31:11 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,17 +1,45 @@
-var app = new Vue({
-    el: "#app", 
+new Vue({
+    el: '#app',
     data: {
-     
         products: [
-            {id: 1, title: "Редис 'Сора'", short_text: 'Ранньостиглий сорт, стійкий до стрілкування', image: 'radish_1.jpg', desc: "Чудовий всесезонний сорт редису, який не утворює пустот і довго зберігає товарний вигляд."},
-            {id: 2, title: "Редис 'Французький сніданок'", short_text: 'Популярний сорт видовженої форми', image: 'radish_2.jpg', desc: "Ранній сорт з циліндричними яскраво-червоними коренеплодами з білим кінчиком. Має слабогострий, приємний смак."},
-            {id: 3, title: "Редис '18 днів'", short_text: 'Ультраранній сорт', image: 'radish_3.jpg', desc: "Один з найшвидших сортів. М'якоть соковита, ніжна, має класичний пікантний смак."},
-            {id: 4, title: "Редис 'Спекота'", short_text: 'Відмінно переносить посуху', image: 'radish_4.jpg', desc: "Ранньостиглий сорт, ідеальний для вирощування в жарких умовах, не схильний до в'янення."},
-            {id: 5, title: "Редис 'Крижана бурулька'", short_text: 'Довгий білий коренеплід', image: 'radish_5.jpg', desc: "Оригінальний пізньостиглий сорт з довгими білими коренеплодами. Смак дуже ніжний та хрусткий."}
+            {
+                id: 1,
+                title: "Цибуля 'Параде'",
+                short_text: "Ранньостиглий сорт, стійкий до пожовтіння пір'я",
+                desc: "Високоврожайний сорт цибулі на перо. Не формує цибулину, швидко відростає після зрізання. Має приємний слабогострий смак.",
+                image: "onion_parade.jpg" 
+            },
+            {
+                id: 2,
+                title: "Цибуля 'Байкал'",
+                short_text: "Популярний сорт з потужним та соковитим стеблом",
+                desc: "Формує міцні прямостоячі листки темно-зеленого кольору. Відрізняється високою стійкістю до хвороб.",
+                image: "onion_baikal.jpg"
+            },
+            {
+                id: 3,
+                title: "Цибуля 'Легіонер'",
+                short_text: "Ультраранній високоврожайний сорт",
+                desc: "Ідеально підходить для швидкого отримання свіжої зелені. Листя ніжне, довго не грубішає.",
+                image: "onion_legionnaire.jpg"
+            },
+            {
+                id: 4,
+                title: "Цибуля 'Вулкан'",
+                short_text: "Відмінно переносить спеку та посушливі умови",
+                desc: "Сорт адаптований до вирощування в умовах високих температур. Зберігає товарний вигляд даже при нестачі вологи.",
+                image: "onion_volcano.jpg"
+            },
+            {
+                id: 5,
+                title: "Цибуля 'Кайгаро'",
+                short_text: "Довге, пружне та вирівняне зелене стебло",
+                desc: "Преміальний сорт для свіжого ринку. Листя має ідеальний товарний вигляд, легко транспортується та довго зберігає свіжість.",
+                image: "onion_kaigaro.jpg"
+            }
         ],
-        product: {}, 
-        btnVisible: 0,
         cart: [],
+        orderSubmitted: false,
         contactFields: {
             name: '',
             company: '',
@@ -25,82 +53,69 @@ var app = new Vue({
             message: '',
             captcha: ''
         },
-        orderSubmitted: false 
+        productId: null
     },
-    mounted: function() {
-        this.getProduct(); 
-        this.checkInCart(); 
-        this.getCart(); 
+    computed: {
+        product() {
+            if (this.productId) {
+                return this.products.find(p => p.id === this.productId) || this.products[0];
+            }
+            return this.products[0];
+        },
+        btnVisible() {
+            // Проверяем, есть ли товар в корзине
+            return this.cart.some(item => item.id === this.product.id);
+        }
+    },
+    // Эта функция срабатывает сразу при загрузке любой страницы
+    mounted() {
+        // Проверяем память браузера: есть ли там уже корзина?
+        const savedCart = localStorage.getItem('cart');
+        if (savedCart) {
+            try {
+                this.cart = JSON.parse(savedCart); // Загружаем товары из памяти
+            } catch(e) {
+                localStorage.removeItem('cart');
+            }
+        }
+
+        const getIdFromHash = () => {
+            if (window.location.hash) {
+                const id = parseInt(window.location.hash.replace('#', ''));
+                if (!isNaN(id)) {
+                    this.productId = id;
+                }
+            }
+        };
+
+        getIdFromHash();
+        window.addEventListener('hashchange', getIdFromHash);
     },
     methods: {
-        getProduct: function() {
-            if(window.location.hash) {
-                var id = window.location.hash.replace('#', '');
-                if(this.products && this.products.length > 0) {
-                    for(let i in this.products) {
-                        if(this.products[i] && this.products[i].id && id == this.products[i].id) {
-                            this.product = this.products[i];
-                        }
-                    }
-                }
+        addToCart(id) {
+            const item = this.products.find(p => p.id === id);
+            if (item && !this.cart.some(p => p.id === id)) {
+                this.cart.push(item);
+                this.saveCart(); 
             }
         },
-        addToCart: function(id) {
-            var localCart = [];
-            if(window.localStorage.getItem('cart')) {
-                localCart = window.localStorage.getItem('cart').split(',');
-            }
-            if(localCart.indexOf(String(id)) == -1) {
-                localCart.push(id);
-                window.localStorage.setItem('cart', localCart.join());
-                this.btnVisible = 1;
-                this.getCart(); 
-            }
+        removeFromCart(id) {
+            this.cart = this.cart.filter(item => item.id !== id);
+            this.saveCart();
         },
-        checkInCart: function() {
-            if(this.product && this.product.id && window.localStorage.getItem('cart') && window.localStorage.getItem('cart').split(',').indexOf(String(this.product.id)) != -1) {
-                this.btnVisible = 1;
-            }
+        saveCart() {
+            
+            localStorage.setItem('cart', JSON.stringify(this.cart));
         },
-        getCart: function() {
-            this.cart = []; 
-            if(window.localStorage.getItem('cart')) {
-                var cartIds = window.localStorage.getItem('cart').split(',');
-                for (var i = 0; i < cartIds.length; i++) {
-                    for (var j = 0; j < this.products.length; j++) {
-                        if (this.products[j].id == cartIds[i]) {
-                            this.cart.push(this.products[j]);
-                        }
-                    }
-                }
+        makeOrder() {
+            const cleanCaptcha = this.contactFields.captcha.replace(/\s/g, '').toUpperCase();
+            if (cleanCaptcha === '7CJ5') {
+                this.orderSubmitted = true;
+                this.cart = []; 
+                this.saveCart(); 
+            } else {
+                alert('Invalid CAPTCHA code. Please try again.');
             }
-        },
-        removeFromCart: function(id) {
-            if(window.localStorage.getItem('cart')) {
-                var cartIds = window.localStorage.getItem('cart').split(',');
-                var index = cartIds.indexOf(String(id));
-                if (index > -1) {
-                    cartIds.splice(index, 1);
-                }
-                
-                if (cartIds.length > 0) {
-                    window.localStorage.setItem('cart', cartIds.join(','));
-                } else {
-                    window.localStorage.removeItem('cart');
-                }
-            }
-            this.getCart();
-      
-            if(this.product.id == id) {
-                this.btnVisible = 0;
-            }
-        },
-        makeOrder: function() {
-            this.orderSubmitted = true;
-            this.cart = [];
-            window.localStorage.removeItem('cart');
-          
-            window.location.hash = '';
         }
     }
 });
